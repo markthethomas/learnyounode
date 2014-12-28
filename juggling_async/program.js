4fd83102515ac8e32683dcb047c8b21b168580e2
Mark Thomas <markthethomas@gmail.com>
2014-12-28 15:50:43 -0800

    finished up and verified

--- juggling_async/program.js
+++ juggling_async/program.js
@@ -1,23 +1,32 @@
-"use strict";
-var http = require('http');
-var _ = require('underscore');
-var bl = require('bl');
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
 
-var urls = process.argv.slice(2);
+var urls = process.argv.slice(2)
 
-// var urls = ['http://example.com', 'http://google.com'];
-
-function getURLsAsync(urls) {
-  for (var i = 0; i < urls.length; i++) {
-    http.get(urls[i], function(res) {
-      res.on('data', function(chunk){
-        console.log(chunk);
-      });
-    }).on('error', function(e) {
-      console.log("Got error: " + e.message);
-    });
-
+function printResults() {
+  for (var i = 0; i < results.length; i++) {
+    console.log(results[i]);
   }
 }
 
-getURLsAsync(urls);
+function httpGET(index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.log(err);
+      results[index] = data.toString()
+      count++
+
+      if (count === urls.length) {
+        printResults();
+      }
+    }))
+  })
+}
+
+
+for (var i = 0; i < urls.length; i++) {
+  httpGET(i);
+}
