4627a0a5cae03612ad3d97c5d8699c08ebe540ba
Mark Thomas <markthethomas@gmail.com>
2014-12-29 09:59:32 -0800

    Created passing code

--- time_server/server.js
+++ time_server/server.js
@@ -1,13 +1,11 @@
 var net = require('net');
-
 var port = process.argv[2]
-
-var server = net.createServer(function (socket) {
-  console.log('client connected');
-  socket.on('end', function() {
-    console.log('client disconnected');
-  });
+var server = net.createServer(function(socket) {
+  var date = new Date();
+  var hourOfDay = ('0' + date.getHours()).slice(-2);
+  var minutesOfDay = ('0' + date.getMinutes()).slice(-2);
+  socket.write(date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getUTCDate() + ' ' + hourOfDay + ':' + minutesOfDay);
+  socket.end();
 });
-
 
 server.listen(port)
