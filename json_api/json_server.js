7400fbfd7b24e2e931885cb5c224ddfb0c0a27b5
Mark Thomas <markthethomas@gmail.com>
2014-12-29 21:49:15 -0800

    Scaffolding out basics

--- json_api/json_server.js
+++ json_api/json_server.js
@@ -1,3 +1,10 @@
 var http = require('http')
 var url = require('url')
 var date = new Date()
+var port = process.argv[2]
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+});
+
+server.listen(port)
