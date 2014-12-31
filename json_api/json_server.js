f3f7076adba8bfd9f8b20144a82f97b36e0b9a3e
Mark Thomas <markthethomas@gmail.com>
2014-12-30 21:36:49 -0800

    Need to parse JSON correctly, but mostly done.

--- json_api/json_server.js
+++ json_api/json_server.js
@@ -1,10 +1,28 @@
 var http = require('http')
 var url = require('url')
-var date = new Date()
+var querystring = require('querystring')
 var port = process.argv[2]
+var enpointBase = '/api/'
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
+var server = http.createServer(function(req, res) {
+  var urlData = url.parse(req.url, true);
+  switch (urlData.pathname) {
+    case enpointBase + 'parsetime':
+      if (urlData.query === undefined) {
+        console.log("oops, you didn't provide a query");
+      }
+
+      var date = new Date(urlData.query.iso)
+      console.log(JSON.parse('{' + "hour:" + date.getHours() + ',' + '"minute:"' + date.getMinutes() + date.getSeconds() + '}' ));
+
+
+      break;
+    case enpointBase + 'unixtime':
+      console.log(urlData.pathname);
+      break;
+    default:
+      console.log("you are not one for the time, are you?");
+  }
 });
 
 server.listen(port)
